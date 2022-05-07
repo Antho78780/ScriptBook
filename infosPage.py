3704318933b33aaaa8489893d5b7f1e63b18790d
Anthony Audrain <anthonyaud78@gmail.com>
2022-05-07 21:57:01 +0200

    ajout la possibilité d'extraire la description des livres

--- infosPage.py
+++ infosPage.py
@@ -2,6 +2,7 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 import time
+from tqdm import tqdm
 
 urlAccueil = "https://books.toscrape.com/"
 pageAccueil = requests.get(urlAccueil)
@@ -16,7 +17,8 @@ number_available = []
 category = []
 review_rating = []
 image_url = []
-en_tete = ["product_page_url","universal_product_code","title","price_including_max","price_excluding_max","number_available","category","review_rating","image_url"]
+product_description = []
+en_tete = ["product_page_url","universal_product_code","title","price_including_max","price_excluding_max","number_available","category","review_rating","image_url", "product_description"]
 
 for li in soup.find("ul", class_="nav-list").ul.find_all("li"):
 	a = li.a.get("href")
@@ -32,6 +34,7 @@ for li in soup.find("ul", class_="nav-list").ul.find_all("li"):
 		soupBook = BeautifulSoup(pageBook.content, "html.parser")
 
 		linkImg = soupBook.img.get("src").strip("../")
+		description = soupBook.find("article", class_="product_page").find_all("p")[3].get_text()
 		categoryBook = soupBook.find("ul", class_="breadcrumb").find_all("li")[2].a.get_text()
 		td = soupBook.find_all("td")
 
@@ -44,6 +47,8 @@ for li in soup.find("ul", class_="nav-list").ul.find_all("li"):
 		number_available.append(td[5].get_text())
 		review_rating.append(td[6].get_text())
 		image_url.append("https://books.toscrape.com/" + linkImg)
+		product_description.append(description)
+		
 	
 	next = soupCategory.find("li", class_="next")
 	if next:
@@ -61,6 +66,7 @@ for li in soup.find("ul", class_="nav-list").ul.find_all("li"):
 					soupNextBook = BeautifulSoup(pageNextBook.content, "html.parser")
 
 					linkNextImgBook = soupNextBook.img.get("src").strip("../")
+					descriptionNextBook = soupNextBook.find("article", class_="product_page").find_all("p")[3].get_text()
 					categoryNextBook = soupNextBook.find("ul", class_="breadcrumb").find_all("li")[2].a.get_text()
 					td = soupNextBook.find_all("td")
 
@@ -73,12 +79,14 @@ for li in soup.find("ul", class_="nav-list").ul.find_all("li"):
 					number_available.append(td[5].get_text())
 					review_rating.append(td[6].get_text())
 					image_url.append("https://books.toscrape.com/" + linkNextImgBook)
-					
-
-with open("data.csv","w") as csv.file:
+					product_description.append(descriptionNextBook)
+	for i in tqdm(product_page_url):
+		time.sleep(0.1)		
+			
+with open("data.csv","w", encoding="utf-8") as csv.file:
 	writer = csv.writer(csv.file, delimiter=",")
 	writer.writerow(en_tete)
-	for product_page_url, universal_product_code, title, price_including_max, price_excluding_max, number_available, category, review_rating, image_url in zip(product_page_url, universal_product_code, title, price_including_max, price_excluding_max, number_available, category, review_rating, image_url):
-		writer.writerow([product_page_url, universal_product_code, title, price_including_max, price_excluding_max, number_available, category, review_rating, image_url])
+	for product_page_url, universal_product_code, title, price_including_max, price_excluding_max, number_available, category, review_rating, image_url, product_description in zip(product_page_url, universal_product_code, title, price_including_max, price_excluding_max, number_available, category, review_rating, image_url, product_description):
+		writer.writerow([product_page_url, universal_product_code, title, price_including_max, price_excluding_max, number_available, category, review_rating, image_url, product_description])
 
 	
