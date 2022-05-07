7d8331dac456465b7b9b60d780b09ff88da51429
Anthony Audrain <anthonyaud78@gmail.com>
2022-05-07 11:43:30 +0200

    ajout la possibilité d'extraire les données des pages suivantes

--- infosPage.py
+++ infosPage.py
@@ -3,9 +3,9 @@ import csv
 from bs4 import BeautifulSoup
 import time
 
-url = "https://books.toscrape.com/"
-page = requests.get(url)
-soup = BeautifulSoup(page.content, "html.parser")
+urlAccueil = "https://books.toscrape.com/"
+pageAccueil = requests.get(urlAccueil)
+soup = BeautifulSoup(pageAccueil.content, "html.parser")
 
 product_page_url = []
 universal_product_code = []
@@ -18,58 +18,67 @@ review_rating = []
 image_url = []
 en_tete = ["product_page_url","universal_product_code","title","price_including_max","price_excluding_max","number_available","category","review_rating","image_url"]
 
-for a in soup.find("ul", class_="nav-list").find("ul").find_all("a"):
-	urlAccueil = "https://books.toscrape.com/" + a["href"]
-	pageAccueil = requests.get(urlAccueil)
-	soup = BeautifulSoup(pageAccueil.content, "html.parser")
-	h3All = soup.find_all("h3")
-	next = soup.find("li", class_="next")
-	print(next)
-
-	for h3 in h3All:
-		urlBook = "https://books.toscrape.com/catalogue/" + h3.find("a")["href"].strip("../")
+for li in soup.find("ul", class_="nav-list").ul.find_all("li"):
+	a = li.a.get("href")
+	urlCategory = "https://books.toscrape.com/" + a
+	pageCategory = requests.get(urlCategory)
+	soupCategory = BeautifulSoup(pageCategory.content, "html.parser")
+	allh3 = soupCategory.find_all("h3")
+	
+	for h3 in allh3:
+		linkBook = h3.a.get("href").strip("../")
+		urlBook = "https://books.toscrape.com/catalogue/" + linkBook
 		pageBook = requests.get(urlBook)
-		soup = BeautifulSoup(pageBook.content, "html.parser")
-		print(urlBook)
+		soupBook = BeautifulSoup(pageBook.content, "html.parser")
 
-		td = soup.find_all("td")
+		linkImg = soupBook.img.get("src").strip("../")
+		categoryBook = soupBook.find("ul", class_="breadcrumb").find_all("li")[2].a.get_text()
+		td = soupBook.find_all("td")
+
 		product_page_url.append(urlBook)
-		universal_product_code.append(td[0].text)
-		title.append(soup.find("h1").text)
-		price_including_max.append(td[2].text)
-		price_excluding_max.append(td[3].text)
-		category.append(soup.find("ul", class_="breadcrumb").find_all("a")[2].text)
-		number_available.append(td[5].text)
-		review_rating.append(td[6].text)
-		image_url.append("https://books.toscrape.com/" + soup.find("img")["src"].strip("../"))
-	"""
-	while next:
-		urlNextPage = "https://books.toscrape.com/" + a["href"].strip("index.html") +  next.find("a")["href"]
-		pageNext = requests.get(urlNextPage)
-		soup = BeautifulSoup(pageNext.content, "html.parser")
-		
-		for h3 in h3All:
-			urlBookNext = "https://books.toscrape.com/catalogue/" + h3.find("a")["href"].strip("../")
-			pageBookNext = requests.get(urlBookNext)
-			soup = BeautifulSoup(pageBookNext.content, "html.parser")
-		
-			td = soup.find_all("td")
-			product_page_url.append(urlBookNext)
-			universal_product_code.append(td[0].text)
-			title.append(soup.find("h1").text)
-			price_including_max.append(td[2].text)
-			price_excluding_max.append(td[3].text)
-			category.append(soup.find("ul", class_="breadcrumb").find_all("a")[2].text)
-			number_available.append(td[5].text)
-			review_rating.append(td[6].text)
-			image_url.append("https://books.toscrape.com/" + soup.find("img")["src"].strip("../"))
-		"""
-"""
+		universal_product_code.append(td[0].get_text())
+		title.append(soupBook.find("h1").get_text())
+		price_including_max.append(td[2].get_text())
+		price_excluding_max.append(td[3].get_text())
+		category.append(categoryBook)
+		number_available.append(td[5].get_text())
+		review_rating.append(td[6].get_text())
+		image_url.append("https://books.toscrape.com/" + linkImg)
+	
+	next = soupCategory.find("li", class_="next")
+	if next:
+		for i in range(2,9):
+			urlPageNext = "https://books.toscrape.com/" + a.strip("index.html") + "page-" + str(i) + ".html"
+			pageNext = requests.get(urlPageNext)
+			if pageNext: 
+				soupPageNext = BeautifulSoup(pageNext.content, "html.parser")
+				allh3 = soupPageNext.find_all("h3")
+				
+				for h3 in allh3:
+					urlNextBook = h3.a.get("href").strip("../")
+					urlNextBook = "https://books.toscrape.com/catalogue/" + urlNextBook
+					pageNextBook = requests.get(urlNextBook)
+					soupNextBook = BeautifulSoup(pageNextBook.content, "html.parser")
+
+					linkNextImgBook = soupNextBook.img.get("src").strip("../")
+					categoryNextBook = soupNextBook.find("ul", class_="breadcrumb").find_all("li")[2].a.get_text()
+					td = soupNextBook.find_all("td")
+
+					product_page_url.append(urlNextBook)
+					universal_product_code.append(td[0].get_text())
+					title.append(soupNextBook.find("h1").get_text())
+					price_including_max.append(td[2].get_text())
+					price_excluding_max.append(td[3].get_text())
+					category.append(categoryNextBook)
+					number_available.append(td[5].get_text())
+					review_rating.append(td[6].get_text())
+					image_url.append("https://books.toscrape.com/" + linkNextImgBook)
+					
+
 with open("data.csv","w") as csv.file:
 	writer = csv.writer(csv.file, delimiter=",")
 	writer.writerow(en_tete)
 	for product_page_url, universal_product_code, title, price_including_max, price_excluding_max, number_available, category, review_rating, image_url in zip(product_page_url, universal_product_code, title, price_including_max, price_excluding_max, number_available, category, review_rating, image_url):
 		writer.writerow([product_page_url, universal_product_code, title, price_including_max, price_excluding_max, number_available, category, review_rating, image_url])
-"""
 
 	
