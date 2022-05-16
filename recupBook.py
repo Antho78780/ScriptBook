42f219227ecc570a25e650987a8ad54ee5a13eba
Anthony Audrain <anthonyaud78@gmail.com>
2022-05-16 03:03:09 +0200

    Ajout du package OS pour la création de dossier

--- recupBook.py
+++ recupBook.py
@@ -1,8 +1,14 @@
-import requests
-import csv
-from bs4 import BeautifulSoup
+import os
 import time
+
+import requests
+from bs4 import BeautifulSoup
 from tqdm import tqdm
+
+
+FILE_PATH = "images"
+if not os.path.exists(FILE_PATH):
+    os.mkdir("images")
 
 url = "https://books.toscrape.com/"
 page = requests.get(url)
@@ -19,16 +25,16 @@ product_page_url = []
 image_url = []
 product_description = []
 en_tete = [
-    "title", "category", "number_available", "price_including_max", "price_excluding_max", "review_rating", "universal_product_code" "product_page_url",
-     "image_url", "product_description"
+    "title", "category", "number_available", "price_including_max", "price_excluding_max", "review_rating",
+    "universal_product_code" "product_page_url", "image_url", "product_description"
 ]
+
 
 def urlSoup(u):
     pageUrl = requests.get(u)
     soupPage = BeautifulSoup(pageUrl.content, "html.parser")
 
     img = soupPage.img.get("src").lstrip("../")
-    print(img)
     urlImg = "https://books.toscrape.com/" + img
     descriptionBook = soupPage.find("article", class_="product_page").find_all("p")[3].get_text().strip()
     categoryBook = soupPage.find("ul", class_="breadcrumb").find_all("li")[2].a.get_text()
@@ -54,7 +60,7 @@ for li in soup.find("ul", class_="nav-list").ul.find_all("li"):
         urlBook = h3.a.get("href").strip("../")
         url = "https://books.toscrape.com/catalogue/" + urlBook
         urlSoup(url)
-
+"""
     next = soup.find("li", class_="next")
     if next:
         for i in range(2, 9):
@@ -67,17 +73,17 @@ for li in soup.find("ul", class_="nav-list").ul.find_all("li"):
                     urlNextBook = h3.a.get("href").strip("../")
                     url = "https://books.toscrape.com/catalogue/" + urlNextBook
                     urlSoup(url)
-
-for i in tqdm(title, desc= "All the books"):
+"""
+for i in tqdm(title, desc="All the books"):
     time.sleep(0.1)  
-
-with open("data.csv","w", encoding="utf-8") as csvFile:
+"""
+with open("data.csv", "w", encoding="utf-8") as csvFile:
     w = csv.writer(csvFile, delimiter=",")
     w.writerow(en_tete)
-    for t, c, na, pim, pem, rr, upc, ppu, iu, pd in zip(
+    for t, c, na, pim, pem, rr, upc, ppu, iu, pd in zip(  
         title, category, number_available, price_including_max,
         price_excluding_max, review_rating, universal_product_code,
         product_page_url, image_url, product_description
-        ):
+    ):
         w.writerow([t, c, na, pim, pem, rr, upc, ppu, iu, pd])
-
+"""
